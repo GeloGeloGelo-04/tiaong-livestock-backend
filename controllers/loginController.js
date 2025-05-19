574ba3109b5fa11a7244fb47eea8ed9fc9c0da31
GeloGeloGelo-04 <geloviejo@gmail.com>
2025-05-19 15:30:34 +0800

    Update loginController.js

--- controllers/loginController.js
+++ controllers/loginController.js
@@ -1,25 +1,28 @@
-const bcrypt = require('bcrypt')
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const Student = require('../model/Student');
 
 const handleLogin = async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) return res.status(400).json({ message: "Username and Password are required" })
+  const { referenceNo, password } = req.body;
+  if (!referenceNo || !password) 
+    return res.status(400).json({ message: "Reference No. and Password are required" });
 
-    const foundUser = await Student.findOne({ username: email }).exec();
-    if (!foundUser) return res.status(401).json({ message: `Account with username '${email}' not found` })
+  const foundUser = await Student.findOne({ username: referenceNo }).exec();
+  if (!foundUser) 
+    return res.status(401).json({ message: `Account with username '${referenceNo}' not found` });
 
-    const match = await bcrypt.compare(password, foundUser.password);
-    if (match == false) return res.status(401).json({ message: `Incorrect password` })
+  const match = await bcrypt.compare(password, foundUser.password);
+  if (!match) 
+    return res.status(401).json({ message: `Incorrect password` });
 
-    try {
-        const fullname = `${foundUser.firstname.charAt(0).toUpperCase()}${foundUser.firstname.slice(1)} ${foundUser.lastname.charAt(0).toUpperCase()}${foundUser.lastname.slice(1)}`
+  try {
+    const fullname = `${foundUser.firstname.charAt(0).toUpperCase()}${foundUser.firstname.slice(1)} ${foundUser.lastname.charAt(0).toUpperCase()}${foundUser.lastname.slice(1)}`;
 
-        res.json({ foundUser, fullname })
-    } catch (error) {
-        console.log(error)
-        res.status(400).json({ "message": error.message })
-    }
-}
+    res.json({ foundUser, fullname });
+  } catch (error) {
+    console.log(error);
+    res.status(400).json({ message: error.message });
+  }
+};
 
-module.exports = { handleLogin }
+module.exports = { handleLogin };
