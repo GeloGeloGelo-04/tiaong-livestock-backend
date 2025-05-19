94045b4e54761c5f373128a519e3400749808391
GeloGeloGelo-04 <geloviejo@gmail.com>
2025-05-19 16:47:39 +0800

    Update resetPassword.js

--- routes/api/resetPassword.js
+++ routes/api/resetPassword.js
@@ -3,13 +3,8 @@ const {
   sendMail,
   updatePwd,
 } = require("../../controllers/resetPassController");
-
 const router = express.Router();
 
-// Route para mag-send ng email code
-router.post("/send-code", sendMail);
-
-// Route para mag-update ng password
-router.put("/", updatePwd);
+router.route("/").post(sendMail).put(updatePwd);
 
 module.exports = router;
