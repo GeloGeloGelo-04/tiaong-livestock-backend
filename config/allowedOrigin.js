2a7b81ea61d5a5ed5e92688f2e28685b6ab729eb
GeloGeloGelo-04 <geloviejo@gmail.com>
2025-06-19 22:08:02 +0800

    Update allowedOrigin.js

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -2,6 +2,8 @@ const allowedOrigins = [
   "https://tiaong-livestock-frontend.vercel.app",
   // "https://vercel.com",
   "http://localhost:5173",
+  "https://tiaong-livestock-frontend-git-master-angelo-f-angelos-projects.vercel.app",
+  "https://tiaong-livestock-frontend-quirzacvm-angelo-f-angelos-projects.vercel.app"
 ];
 
 module.exports = allowedOrigins;
