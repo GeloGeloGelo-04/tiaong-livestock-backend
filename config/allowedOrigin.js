0e3597984d058df3360bc44c3807e62619ee107e
GeloGeloGelo-04 <geloviejo@gmail.com>
2025-06-19 22:11:35 +0800

    Update allowedOrigin.js

--- config/allowedOrigin.js
+++ config/allowedOrigin.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://tiaong-livestock-frontend.vercel.app",
+    "https://tiaong-livestock-frontend-kohl.vercel.app",
   // "https://vercel.com",
   "http://localhost:5173",
   "https://tiaong-livestock-frontend-git-master-angelo-f-angelos-projects.vercel.app",
